0f442f2c7dbcff89babd112395370502516c7c4b
yanuar.maulana <eragon.shapira10@gmail.com>
2022-08-08 19:26:39 +0700

    first

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Get("/:name", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello " + ctx.Params("name"))
+	})
+	app.Listen(":3000")
+}
